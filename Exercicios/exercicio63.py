a22adf1ba6d76b59581181c751f9fba8c28cbe00
Priscila Lobo <priscilafernandeslobo@hotmail.com>
2020-04-16 16:12:35 -0300

    Exercicio 63

---
+++ Exercicios/exercicio63.py
@@ -0,0 +1,20 @@
+#Exercício Python 063: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os 
+# N primeiros elementos de uma Sequência de Fibonacci. 
+
+termo = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+print('-' *30)
+print(f'{t1} -> {t2}', end='')
+cont = 3
+while cont <= termo:
+    t3 = t1+t2
+    print(f'-> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print('-> Fim')
+
+
+
+
